d970556c288adc32883bc7b78bd09bbd519776a2
Anthony Powell <anthonypowell@hey.com>
2023-04-07 15:21:18 -0400

    Rename the remember command to ask

--- apps/bot/src/discord/interactions.ts
+++ apps/bot/src/discord/interactions.ts
@@ -2,20 +2,12 @@ import { listCommand } from './commands/list.js';
 import { joinCommand } from './commands/join.js';
 import { leaveCommand } from './commands/leave.js';
 import { askCommand } from './commands/ask.js';
-import { rememberCommand } from './commands/remember.js';
 import { replyToMeetingMessageHandler } from './messageHandlers/replyToMeetingMessage.js';
 import type { Command } from './createCommand.js';
 import { renameCommand } from './commands/rename.js';
 
 // Add new commands here
-const commandInteractions = [
-	joinCommand,
-	leaveCommand,
-	askCommand,
-	rememberCommand,
-	renameCommand,
-	listCommand,
-] as const;
+const commandInteractions = [joinCommand, leaveCommand, askCommand, renameCommand, listCommand] as const;
 
 export const interactionCommandHandlers = new Map<string, Command>();
 
