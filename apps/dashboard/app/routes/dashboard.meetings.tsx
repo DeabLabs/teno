3341aca026be38b4f94cbab2b474946c6fb61f67
Anthony Powell <anthonypowell@hey.com>
2023-04-09 15:37:34 -0400

    Improve Meeting table

--- apps/dashboard/app/routes/dashboard.meetings.tsx
+++ apps/dashboard/app/routes/dashboard.meetings.tsx
@@ -32,7 +32,6 @@ export const action = async ({ request }: LoaderArgs) => {
 				.filter((id) => !isNaN(id));
 
 			if (IDsToDelete && IDsToDelete.length > 0) {
-				// TODO call deleteAuthoredMeetingsById with a redisClient deletion function
 				await meetingQueries.deleteAuthoredMeetingsById(
 					prisma,
 					(transcriptKeys) => transcriptQueries.batchDeleteTranscripts(redis, { transcriptKeys }),
@@ -84,92 +83,120 @@ const DashboardMeetings = () => {
 	const mainBg = 'bg-gray-900';
 	const mainText = 'text-gray-100';
 	const secondaryText = 'text-white';
+	const stickyHeader = 'sticky top-16 z-10 bg-gray-900';
 
 	return (
-		<Form className="w-full mb-4 rounded" method="post" replace>
-			<fieldset className={clsx('flow-root mt-6')} disabled={loading}>
-				<div className="overflow-x-auto">
-					<div className={clsx('inline-block min-w-full align-middle rounded', mainBg)}>
-						<div className="relative">
-							{(selectedMeeting.length > 0 || loading) && (
-								<div className={clsx('absolute left-14 top-0 flex h-12 items-center space-x-3 sm:left-12', mainBg)}>
-									<Button name="_action" value="delete" type="submit" variant={'destructive'} size={'sm'}>
-										Delete Selected
-									</Button>
-									{loading && <Loader2 className="animate-spin" />}
-								</div>
-							)}
-							<table className="min-w-full table-fixed divide-y divide-gray-800">
-								<thead>
-									<tr className={clsx(mainText)}>
-										<th scope="col" className="relative px-7 sm:w-12 sm:px-6">
-											<input
-												type="checkbox"
-												className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-												ref={checkbox}
-												checked={checked}
-												onChange={toggleAll}
-											/>
-										</th>
-										<th scope="col" className={'min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold'}>
-											Name
-										</th>
-										<th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold">
-											Created At
-										</th>
-										<th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold">
-											Locked
-										</th>
-										<th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold">
-											Active
-										</th>
-									</tr>
-								</thead>
-								<tbody className={clsx('divide-y divide-gray-800', mainBg)}>
-									{authoredMeetings.map((meeting) => (
-										<tr
-											key={meeting.id}
-											className={clsx(selectedMeeting.includes(meeting) && 'bg-gray-800', secondaryText)}
-										>
-											<td className="relative px-7 sm:w-12 sm:px-6">
-												{selectedMeeting.includes(meeting) && (
-													<div className="absolute inset-y-0 left-0 w-1 bg-gray-600" />
-												)}
+		<div className="flex flex-col w-full gap-8">
+			<div className="sm:px-4 mt-8">
+				<div className="sm:flex sm:items-center">
+					<div className="sm:flex-auto">
+						<h1 className="text-base font-semibold leading-6 text-white">Authored Meetings</h1>
+						<p className="mt-2 text-sm text-gray-300">
+							A list of all the meetings that you have authored, across all of your guilds.
+						</p>
+						<p className="text-sm text-red-300">
+							Deleting a meeting here will delete it for all attendees. <b>You cannot undo this action.</b>
+						</p>
+					</div>
+				</div>
+			</div>
+			<Form className="w-full mb-4 rounded" method="post" replace>
+				<fieldset className={clsx('flow-root')} disabled={loading}>
+					<div className="">
+						<div className={clsx('inline-block min-w-full align-middle rounded', mainBg)}>
+							<div className="relative">
+								<table className="min-w-full table-fixed divide-y divide-gray-800">
+									<thead>
+										<tr className={clsx(mainText)}>
+											<th scope="col" className={clsx(stickyHeader, 'relative px-7 sm:w-12 sm:px-6')}>
 												<input
 													type="checkbox"
 													className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-													name="selectedMeeting"
-													value={meeting.id}
-													checked={selectedMeeting.includes(meeting)}
-													onChange={(e) =>
-														setSelectedMeeting(
-															e.target.checked
-																? [...selectedMeeting, meeting]
-																: selectedMeeting.filter((p) => p !== meeting),
-														)
-													}
+													ref={checkbox}
+													checked={checked}
+													onChange={toggleAll}
 												/>
-											</td>
-											<td
-												className={clsx(
-													'whitespace-nowrap py-4 pr-3 text-sm font-medium',
-													selectedMeeting.includes(meeting) ? 'text-indigo-200' : secondaryText,
-												)}
+											</th>
+											<th
+												scope="col"
+												className={clsx(stickyHeader, 'min-w-[12rem] pr-3 text-left text-sm font-semibold')}
 											>
-												{meeting.name}
-											</td>
-											<td className="whitespace-nowrap px-3 py-4 text-sm">{meeting.createdAt}</td>
-											<td className="whitespace-nowrap px-3 py-4 text-sm">{String(meeting.locked)}</td>
-											<td className="whitespace-nowrap px-3 py-4 text-sm">{String(meeting.active)}</td>
+												<div className="flex h-full w-full items-center gap-5">
+													Name
+													{(selectedMeeting.length > 0 || loading) && (
+														<div className={clsx('flex h-full items-center', mainBg)}>
+															<Button
+																name="_action"
+																value="delete"
+																type="submit"
+																variant={'destructive'}
+																size={'sm'}
+																className="h-8"
+															>
+																Delete {selectedMeeting.length} Meeting{selectedMeeting.length > 1 ? 's' : ''}
+															</Button>
+															{loading && <Loader2 className="animate-spin" />}
+														</div>
+													)}
+												</div>
+											</th>
+											<th scope="col" className={clsx(stickyHeader, 'px-3 py-3.5 text-left text-sm font-semibold')}>
+												Created At
+											</th>
+											<th scope="col" className={clsx(stickyHeader, 'px-3 py-3.5 text-left text-sm font-semibold')}>
+												Locked
+											</th>
+											<th scope="col" className={clsx(stickyHeader, 'px-3 py-3.5 text-left text-sm font-semibold')}>
+												Active
+											</th>
 										</tr>
-									))}
-								</tbody>
-							</table>
+									</thead>
+									<tbody className={clsx('divide-y divide-gray-800', mainBg)}>
+										{authoredMeetings.map((meeting) => (
+											<tr
+												key={meeting.id}
+												className={clsx(selectedMeeting.includes(meeting) && 'bg-gray-800', secondaryText)}
+											>
+												<td className="relative px-7 sm:w-12 sm:px-6">
+													{selectedMeeting.includes(meeting) && (
+														<div className="absolute inset-y-0 left-0 w-1 bg-gray-600" />
+													)}
+													<input
+														type="checkbox"
+														className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+														name="selectedMeeting"
+														value={meeting.id}
+														checked={selectedMeeting.includes(meeting)}
+														onChange={(e) =>
+															setSelectedMeeting(
+																e.target.checked
+																	? [...selectedMeeting, meeting]
+																	: selectedMeeting.filter((p) => p !== meeting),
+															)
+														}
+													/>
+												</td>
+												<td
+													className={clsx(
+														'whitespace-nowrap py-4 pr-3 text-sm font-medium',
+														selectedMeeting.includes(meeting) ? 'text-indigo-200' : secondaryText,
+													)}
+												>
+													{meeting.name}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm">{meeting.createdAt}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm">{String(meeting.locked)}</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm">{String(meeting.active)}</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
 						</div>
 					</div>
-				</div>
-			</fieldset>
-		</Form>
+				</fieldset>
+			</Form>
+		</div>
 	);
 };
 
