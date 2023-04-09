ba7de5ad0d550f7b1be44acf1c0a5a25e6a854d4
Anthony Powell <anthonypowell@hey.com>
2023-04-09 13:31:31 -0400

    Rename Icons.logo

--- apps/dashboard/app/components/Icons.tsx
+++ apps/dashboard/app/components/Icons.tsx
@@ -1,5 +1,5 @@
 import { LucideTable } from 'lucide-react';
 
 export const Icons = {
-	logo: LucideTable,
-};
+	Logo: LucideTable,
+} as const;
--- apps/dashboard/app/routes/login.tsx
+++ apps/dashboard/app/routes/login.tsx
@@ -7,7 +7,7 @@ const Login = () => {
 	return (
 		<div className="flex flex-col min-w-screen min-h-screen justify-center items-center">
 			<h1 className="flex p-4 gap-2 mb-8">
-				<Icons.logo />
+				<Icons.Logo />
 				Teno Dashboard
 			</h1>
 			<Form action="/auth/discord" method="post">
