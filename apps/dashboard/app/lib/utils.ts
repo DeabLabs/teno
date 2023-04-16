8ab17e36281591293a7711aeb0356c492db4feb8
Anthony Powell <anthonypowell@hey.com>
2023-04-16 10:05:12 -0400

    Refactor meeting table into new component

--- apps/dashboard/app/lib/utils.ts
+++ apps/dashboard/app/lib/utils.ts
@@ -1,7 +1,23 @@
 import type { ClassValue } from 'clsx';
 import { clsx } from 'clsx';
+import { formatDuration, intervalToDuration } from 'date-fns';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const formatMeetingDuration = (ms: number) => {
+	const duration = intervalToDuration({ start: 0, end: ms });
+
+	const zeroPad = (num: number) => String(num).padStart(2, '0');
+
+	return formatDuration(duration, {
+		format: ['hours', 'minutes', 'seconds'],
+		zero: true,
+		delimiter: ':',
+		locale: {
+			formatDistance: (_token, count) => zeroPad(count),
+		},
+	});
+};
