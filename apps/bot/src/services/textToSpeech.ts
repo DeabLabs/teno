ed2698bb68e773b12e95410d0a33fa71199ebb93
Blind Mansion <blindmansion@gmail.com>
2023-04-22 13:29:22 -0400

    Refactor tts to be more service-modular

--- apps/bot/src/services/textToSpeech.ts
+++ apps/bot/src/services/textToSpeech.ts
@@ -1,32 +1,62 @@
 import { Readable } from 'stream';
-import { PassThrough } from 'stream';
 
 import type { VoiceConnection } from '@discordjs/voice';
 import { AudioPlayerStatus, createAudioPlayer, createAudioResource } from '@discordjs/voice';
-import invariant from 'tiny-invariant';
 import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
+import opus from '@discordjs/opus';
 
 import { Config } from '@/config.js';
 
-export async function textToSpeech(
-	voiceId: string,
-	text: string,
-	apiKey: string,
-	stability: number,
-	similarity_boost: number,
-): Promise<ArrayBuffer> {
-	const url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`;
+const { OpusEncoder } = opus;
+
+type TTSResult = {
+	arrayBuffer: ArrayBuffer;
+	audioFormat: string;
+};
+
+type ElevenLabsParams = {
+	connection: VoiceConnection | undefined;
+	text: string;
+	service: 'elevenlabs';
+	apiKey: string;
+	voiceId: string;
+	stability?: number;
+	similarity_boost?: number;
+};
+
+type AzureParams = {
+	connection: VoiceConnection | undefined;
+	text: string;
+	service: 'azure';
+	apiKey: string;
+};
+
+type TTSParams = ElevenLabsParams | AzureParams;
+
+async function textToSpeech(params: TTSParams): Promise<TTSResult> {
+	switch (params.service) {
+		case 'elevenlabs':
+			return await getElevenLabsTTS(params);
+		case 'azure':
+			return await getAzureTTS(params);
+		default:
+			throw new Error('Invalid TTS service');
+	}
+}
+
+async function getElevenLabsTTS(params: ElevenLabsParams): Promise<TTSResult> {
+	const url = `https://api.elevenlabs.io/v1/text-to-speech/${params.voiceId}/stream`;
 	const headers = {
 		'Content-Type': 'application/json',
-		'xi-api-key': apiKey,
+		'xi-api-key': params.apiKey,
 		accept: 'audio/mpeg',
 	};
 
 	const body = JSON.stringify({
-		text,
+		text: params.text,
 		voice_settings: {
-			stability,
-			similarity_boost,
+			stability: params.stability || 0.3,
+			similarity_boost: params.similarity_boost || 0.8,
 		},
 	});
 
@@ -41,10 +71,46 @@ export async function textToSpeech(
 	}
 
 	const arrayBuffer = await response.arrayBuffer();
-	return arrayBuffer;
+	return {
+		arrayBuffer,
+		audioFormat: 'audio/mpeg',
+	};
 }
 
-export async function playAudioBuffer(arrayBuffer: ArrayBuffer, connection: VoiceConnection): Promise<void> {
+const azureSpeechKey = Config.AZURE_SPEECH_KEY;
+const azureSpeechRegion = Config.AZURE_SPEECH_REGION;
+
+const azureSpeechConfig = sdk.SpeechConfig.fromSubscription(azureSpeechKey, azureSpeechRegion);
+
+// Set the output format
+azureSpeechConfig.speechSynthesisOutputFormat = sdk.SpeechSynthesisOutputFormat.Ogg48Khz16BitMonoOpus;
+
+const speechSynthesizer = new sdk.SpeechSynthesizer(azureSpeechConfig);
+
+async function getAzureTTS(params: AzureParams): Promise<TTSResult> {
+	return new Promise<TTSResult>((resolve, reject) => {
+		speechSynthesizer.speakTextAsync(
+			params.text,
+			(result) => {
+				const { audioData } = result;
+				resolve({
+					arrayBuffer: audioData,
+					audioFormat: 'audio/ogg',
+				});
+			},
+			(error) => {
+				console.log(error);
+				reject(error);
+			},
+		);
+	});
+}
+
+async function playAudioBuffer(
+	arrayBuffer: ArrayBuffer,
+	audioFormat: string,
+	connection: VoiceConnection,
+): Promise<void> {
 	const buffer = Buffer.from(new Uint8Array(arrayBuffer));
 	const bufferStream = Readable.from(buffer);
 	const audioResource = createAudioResource(bufferStream);
@@ -65,58 +131,15 @@ export async function playAudioBuffer(arrayBuffer: ArrayBuffer, connection: Voic
 	});
 }
 
-export async function playTextToSpeech({
-	connection,
-	text,
-	apiKey,
-	voiceId,
-}: {
-	apiKey: string;
-	voiceId: string;
-	connection: VoiceConnection | undefined;
-	text: string;
-}): Promise<void> {
-	const defaultStability = 0.3;
-	const defaultSimilarityBoost = 0.8;
-
-	invariant(apiKey, 'Eleven labs API key is required');
-	invariant(voiceId, 'Eleven labs voice ID is required');
-
+export async function playTextToSpeech(params: TTSParams): Promise<void> {
 	try {
-		if (connection) {
-			//const buffer = await textToSpeech(voiceId, text, apiKey, defaultStability, defaultSimilarityBoost);
-			const buffer = await synthesizeSpeech(text);
-			await playAudioBuffer(buffer, connection);
+		if (params.connection) {
+			const ttsResult = await textToSpeech(params);
+			await playAudioBuffer(ttsResult.arrayBuffer, ttsResult.audioFormat, params.connection);
 		} else {
 			console.error('No voice connection found for the meeting');
 		}
 	} catch (error) {
-		console.error('Error error playing text to speech:', error);
+		console.error('Error playing text to speech:', error);
 	}
-}
-
-const speechKey = Config.SPEECH_KEY;
-const speechRegion = Config.SPEECH_REGION;
-
-const speechConfig = sdk.SpeechConfig.fromSubscription(speechKey, speechRegion);
-
-// Set the output format
-speechConfig.speechSynthesisOutputFormat = sdk.SpeechSynthesisOutputFormat.Ogg48Khz16BitMonoOpus;
-
-const speechSynthesizer = new sdk.SpeechSynthesizer(speechConfig);
-
-function synthesizeSpeech(text: string): Promise<ArrayBuffer> {
-	return new Promise<ArrayBuffer>((resolve, reject) => {
-		speechSynthesizer.speakTextAsync(
-			text,
-			(result) => {
-				const { audioData } = result;
-				resolve(audioData);
-			},
-			(error) => {
-				console.log(error);
-				reject(error);
-			},
-		);
-	});
 }
